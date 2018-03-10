fc0c729b9f7142b19a3e108ea6046debf6c62bf1
Bill Kohler <bkohler4@gmail.com>
2018-03-10 18:35:48 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,30 @@ User.create!(name: 'Bill Kohler',
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+
+User.create!(name: 'Admin User',
+            email: 'admin@example.com',
+            password: 'foobar',
+            password_confirmation: 'foobar',
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
+
+User.create!(name: 'Standard User',
+            email: 'user@example.com',
+            password: 'foobar',
+            password_confirmation: 'foobar',
+            admin: false,
+            activated: true,
+            activated_at: Time.zone.now)
+
+97.times do |n|
+  name = Faker::TwinPeaks.character
+  password = 'foobar'
+  User.create!(name: name,
+               email: "example#{n+1}@thelodge.com",
+               password: password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
