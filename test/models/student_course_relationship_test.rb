c4ad1e794fbc31b26ddf7f09db7be2d43f94b45b
Bill Kohler <bill@thekurlandagency.com>
2018-03-14 13:09:16 -0400

    validate grade

--- test/models/student_course_relationship_test.rb
+++ test/models/student_course_relationship_test.rb
@@ -9,9 +9,23 @@ class StudentCourseRelationshipTest < ActiveSupport::TestCase
     @admin = users(:admin)
     @bio = courses(:biology)
     @math = courses(:math)
+    @relationship = StudentCourseRelationship.create!(user_id: @student.id,
+                                                      course_id: @math.id)
   end
 
   test 'grade should be invalid' do
+    assert @relationship.valid?
+    new_relationship = StudentCourseRelationship.new(user_id: @student_2.id,
+                                                     course_id: @math.id)
+    # Grade can't be greater than 4
+    new_relationship.grade = 6.2
+    assert_not new_relationship.valid?
+    # Grade can't be less than 0
+    new_relationship.grade = -1.4
+    assert_not new_relationship.valid?
+    #Grade is the correct range
+    new_relationship.grade = 3.33
+    assert new_relationship.valid?
   end
 
 
