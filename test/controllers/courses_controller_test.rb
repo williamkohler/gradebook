0c118245bdc3b9ea73f74ac2e5801b8a3dbf06a1
Bill Kohler <bill@thekurlandagency.com>
2018-03-14 13:03:55 -0400

    update test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get courses_index_url
-    assert_response :success
-  end
-
 end
